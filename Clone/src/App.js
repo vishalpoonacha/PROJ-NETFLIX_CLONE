fc1c8456267c988fc90027c7237dd31004f4f1c9
SUSHANT CHAUHAN <51734530+sushantpeace10@users.noreply.github.com>
2020-12-06 11:42:19 +0530

    Add files via upload

---
+++ Clone/src/App.js
@@ -0,0 +1,31 @@
+import React from "react";
+import "./App.css";
+import Row from "./Row";
+import requests from "./request";
+import Banner from "./Banner";
+import Nav from "./Nav";
+ 
+
+function App() {
+  return (
+    <div className="App">
+      <Nav/>
+    <Banner/>
+      <Row
+        title="NETFLIX ORIGINALS "
+        fetchUrl={requests.fetchNetflixOriginals}
+        isLargeRow={true}
+      />
+      <Row title="Trending Now" fetchUrl={requests.fetchTrending} isLargeRow={true}/>
+      <Row title="Top Rated" fetchUrl={requests.fetchTopRated}isLargeRow={true} />
+      <Row title="Action Movies" fetchUrl={requests.fetchActionMovies} isLargeRow={true}/>
+      <Row title="Comedy Movies" fetchUrl={requests.fetchComedyMovies}isLargeRow={true} />
+      <Row title="Horror Movies" fetchUrl={requests.fetchHorrorMovies} isLargeRow={true}/>
+      <Row title="Romance Movies" fetchUrl={requests.fetchRomanceMovies}isLargeRow={true} />
+      <Row title="Documentaries" fetchUrl={requests.fetchDocumentaries}isLargeRow={true} />
+      
+    </div>
+  );
+}
+// Park cursor here ==>>> 
+export default App;
